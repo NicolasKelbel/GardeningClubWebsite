8bcde75bcf3a8f30a839968217c885bc45408bb5
Blank432 <aaravgarg@live.com>
2024-10-09 19:43:13 +0200

    structure backend

--- server/server.py
+++ server/server.py
@@ -7,3 +7,27 @@ def test():
     return {
         "test": "successful"
     }
+
+@app.route("/home")
+def home():
+    return {
+        "": ""
+    }
+
+@app.route("/funding")
+def funding():
+    return {
+        "": ""
+    }
+
+@app.route("/blogs")
+def blogs():
+    return {
+        "": ""
+    }
+
+@app.route("/articles")
+def articles():
+    return {
+        "": ""
+    }
