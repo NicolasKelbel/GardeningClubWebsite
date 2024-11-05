2d3b0991ab67d6addd1958e0ef43ebc2a9ef051d
NicolasKelbel <nicolas.kelbel@outlook.de>
2024-11-05 20:47:07 +0100

    changed homepage design with Amy

--- client/src/components/navbar/Navbar.tsx
+++ client/src/components/navbar/Navbar.tsx
@@ -11,21 +11,23 @@ const Navbar = ({ isHomePage }: { isHomePage: boolean }) => {
 
   return (
     <div className='navbar' id={isHomePage ? 'homepage' : ''}>
+        <div className='header__logoContainer'>
+            <img src="https://placehold.co/300x300" alt="" className='header__logo'/>
+        </div>
+        <div className='header__navLinkContainer'>
         <NavLink to='/' className={({isActive, isPending}:NavLinkClassTypes) => 
             isPending ? "navLink navLink--pending" : isActive ? "navLink navLink--isActive" : "navLink"
         } >Home</NavLink>
         <NavLink to='/blogs' className={({isActive, isPending}:NavLinkClassTypes) => 
             isPending ? "navLink navLink--pending" : isActive ? "navLink navLink--isActive" : "navLink"
         } >Blogs</NavLink>
-        <div className='header__logoContainer'>
-            <img src="https://placehold.co/100x80" alt="" />
-        </div>
         <NavLink to='/articles' className={({isActive, isPending}:NavLinkClassTypes) => 
             isPending ? "navLink navLink--pending" : isActive ? "navLink navLink--isActive" : "navLink"
         } >Articles</NavLink>
         <NavLink to='/contact' className={({isActive, isPending}:NavLinkClassTypes) => 
             isPending ? "navLink navLink--pending" : isActive ? "navLink navLink--isActive" : "navLink"
         } >Contact</NavLink>
+        </div>
     </div>
   )
 }
