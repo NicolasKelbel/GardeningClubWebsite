3cd2aec21f72a1f2d7f672f57da33aec035a2464
NicolasKelbel <nicolas.kelbel@outlook.de>
2024-11-14 23:26:49 +0100

    Continued developing about us section

--- client/src/components/aboutUs/AboutUs.tsx
+++ client/src/components/aboutUs/AboutUs.tsx
@@ -4,13 +4,44 @@ import './aboutUs.css'
 const AboutUs = () => {
   return (
     <div className='aboutUs'>
-        <div className='aboutUs__leftColumn'>
+        <div className='aboutUs__column'>
             <h1 className='aboutUs__title'>About Us</h1>
+            <div className='aboutUs__item'>
+              <div className='aboutUs__textOutsideContainer'>
+                <div className='aboutUs__textInsideContainer'>
+                  <p className='aboutUs__text'>fkjdslajfkldjsaökfjdkslajfkdsjakfldwadwa fkjdslajfkldjsaökfjdkslajfkdsjakfldwadwa fkjdslajfkldjsaökfjdkslajfkdsjakfldwadwa fkjdslajfkldjsaökfjdkslajfkdsjakfldwadwa fkjdslajfkldjsaökfjdkslajfkdsjakfldwadwa fkjdslajfkldjsaökfjdkslajfkdsjakfldwadwa</p>
+                </div>
+              </div>
+              <div className='aboutUs__horizontalLine'></div>
+            </div>
+            <div className='aboutUs__item'>
+              <div className='aboutUs__textOutsideContainer'>
+                <div className='aboutUs__textInsideContainer'>
+                  <p className='aboutUs__text'>fkjdslajfkldjsaökfjdkslajfkdsjakfldwadwa fkjdslajfkldjsaökfjdkslajfkdsjakfldwadwa fkjdslajfkldjsaökfjdkslajfkdsjakfldwadwa fkjdslajfkldjsaökfjdkslajfkdsjakfldwadwa fkjdslajfkldjsaökfjdkslajfkdsjakfldwadwa fkjdslajfkldjsaökfjdkslajfkdsjakfldwadwa</p>
+                </div>
+              </div>
+              <div className='aboutUs__horizontalLine'></div>
+            </div>
         </div>
         <div className='aboutUs__verticalLine'>
         </div>
-        <div className='aboutUs__rightColumn'>
-
+        <div className='aboutUs__column'>
+          <div className='aboutUs__item' id='aboutUs__itemRight'>
+              <div className='aboutUs__horizontalLine'></div>
+              <div className='aboutUs__textOutsideContainer'>
+                <div className='aboutUs__textInsideContainer'>
+                  <p className='aboutUs__text'>fkjdslajfkldjsaökfjdkslajfkdsjakfldwadwa fkjdslajfkldjsaökfjdkslajfkdsjakfldwadwa fkjdslajfkldjsaökfjdkslajfkdsjakfldwadwa fkjdslajfkldjsaökfjdkslajfkdsjakfldwadwa fkjdslajfkldjsaökfjdkslajfkdsjakfldwadwa fkjdslajfkldjsaökfjdkslajfkdsjakfldwadwa</p>
+                </div>
+              </div>
+            </div>
+            <div className='aboutUs__item' id='aboutUs__itemRight'>
+              <div className='aboutUs__horizontalLine'></div>
+              <div className='aboutUs__textOutsideContainer'>
+                <div className='aboutUs__textInsideContainer'>
+                  <p className='aboutUs__text'>fkjdslajfkldjsaökfjdkslajfkdsjakfldwadwa fkjdslajfkldjsaökfjdkslajfkdsjakfldwadwa fkjdslajfkldjsaökfjdkslajfkdsjakfldwadwa fkjdslajfkldjsaökfjdkslajfkdsjakfldwadwa fkjdslajfkldjsaökfjdkslajfkdsjakfldwadwa fkjdslajfkldjsaökfjdkslajfkdsjakfldwadwa</p>
+                </div>
+              </div>
+            </div>
         </div>
 
     </div>
