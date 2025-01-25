7719782d6f145e6fa83516b78902c6886590c7c2
NicolasKelbel <nicolas.kelbel@outlook.de>
2025-01-25 21:59:46 +0100

    added universal about us page

--- client/src/components/aboutUs/AboutUs.tsx
+++ client/src/components/aboutUs/AboutUs.tsx
@@ -1,47 +1,47 @@
 import React from 'react'
 import './aboutUs.css'
 
-const AboutUs = () => {
+type Item = {
+  text: string; 
+};
+
+type items = {
+  items: Item[]; 
+};
+
+const AboutUs = ({items}:items) => {
+
+  const leftItems = items.filter((_, index) => index % 2 === 0);
+  const rightItems = items.filter((_, index) => index % 2 !== 0);
+
   return (
     <div className='aboutUs'>
         <div className='aboutUs__column'>
             <h1 className='aboutUs__title'>About Us</h1>
-            <div className='aboutUs__item'>
-              <div className='aboutUs__textOutsideContainer'>
-                <div className='aboutUs__textInsideContainer'>
-                  <p className='aboutUs__text'>fkjdslajfkldjsaökf fkjdslajfkldjsaökf fkjdslajfkldjsaökf fkjdslajfkldjsaökf</p>
+            {leftItems.map((item, index) => (
+              <div key={index} className='aboutUs__item'>
+                <div className='aboutUs__textOutsideContainer'>
+                  <div className='aboutUs__textInsideContainer'>
+                    <p className='aboutUs__text'>{item.text}</p>
+                  </div>
                 </div>
+                <div className='aboutUs__horizontalLine'></div>
               </div>
-              <div className='aboutUs__horizontalLine'></div>
-            </div>
-            <div className='aboutUs__item'>
-              <div className='aboutUs__textOutsideContainer'>
-                <div className='aboutUs__textInsideContainer'>
-                  <p className='aboutUs__text'>fkjdslajfkldjsaökf fkjdslajfkldjsaökf fkjdslajfkldjsaökf fkjdslajfkldjsaökf</p>
-                </div>
-              </div>
-              <div className='aboutUs__horizontalLine'></div>
-            </div>
+            ))}
         </div>
         <div className='aboutUs__verticalLine'>
         </div>
         <div className='aboutUs__column'>
-          <div className='aboutUs__itemRight aboutUs__item' id='aboutUs__firstRight'>
-              <div className='aboutUs__horizontalLine'></div>
-              <div className='aboutUs__textOutsideContainer'>
-                <div className='aboutUs__textInsideContainer'>
-                  <p className='aboutUs__text'>fkjdslajfkldjsaökf fkjdslajfkldjsaökf fkjdslajfkldjsaökf fkjdslajfkldjsaökf</p>
+          {rightItems.map((item, index) => (
+                <div key={index} className='aboutUs__itemRight aboutUs__item' id={index === 0 ? 'aboutUs__firstRight' : ''}>
+                  <div className='aboutUs__horizontalLine'></div>
+                  <div className='aboutUs__textOutsideContainer' >
+                    <div className='aboutUs__textInsideContainer'>
+                      <p className='aboutUs__text'>{item.text}</p>
+                    </div>
+                  </div>
                 </div>
-              </div>
-            </div>
-            <div className='aboutUs__itemRight aboutUs__item'>
-              <div className='aboutUs__horizontalLine'></div>
-              <div className='aboutUs__textOutsideContainer'>
-                <div className='aboutUs__textInsideContainer'>
-                  <p className='aboutUs__text'>fkjdslajfkldjsaökf fkjdslajfkldjsaökf fkjdslajfkldjsaökf fkjdslajfkldjsaökf</p>
-                </div>
-              </div>
-            </div>
+              ))}
         </div>
 
     </div>
--- client/src/pages/homePage/HomePage.tsx
+++ client/src/pages/homePage/HomePage.tsx
@@ -5,12 +5,24 @@ import AboutUs from '../../components/aboutUs/AboutUs'
 import PlaceHolderWithLine from '../../components/placeholders/placeholderWIthLine/PlaceholderWithLine'
 import PlaceHolderWithEmptyCircle from '../../components/placeholders/placeHolderWithEmptyCircle/PlaceHolderWithEmptyCircle'
 
+type Item = {
+  text: string; // Define the shape of each item in the array
+};
+
 const HomePage = () => {
+
+  const data: Item[] = [
+    { text: 'The gardening club is an association created by the students at the SISS (State International School Seeheim), which aims to promote environmental sustainability, by creating a green space within the school. ' },
+    { text: 'At the same time, its members are advocating to prioritize native species of plants over invasive ones, to raise awareness over the issues this creates.' },
+    { text: 'This has so far been achieved by introducing more plants into the learning spaces. This action involves filling the classrooms with plants and an inside gravel pit with large potted shrubs and small trees.' },
+    { text: 'Future projects will include creating a vegetable or herb garden for the school to access, which will help gauge interest in sustainable agriculture within the younger years.' },
+  ];
+
   return (
     <div className='homepage'>
         <Welcome />
-        <PlaceHolderWithLine height={'20px'}/>
-        <AboutUs />
+        <PlaceHolderWithLine height={'10px'}/>
+        <AboutUs items={data}/>
         <PlaceHolderWithLine height={'60px'}/>
         <PlaceHolderWithEmptyCircle />
     </div>
